813604ad814b27666f861912834f0924927f31a7
HydroOx1d <n-esenov44@mail.ru>
2023-05-05 20:23:43 +0600

    update: update rect

--- src/components/toolbar/ToolBar.jsx
+++ src/components/toolbar/ToolBar.jsx
@@ -10,6 +10,7 @@ import save from '@/assets/img/save.svg'
 import toolbarState from '../../store/toolbarState'
 import Brush from '../../tools/Brush'
 import canvasState from '../../store/canvasState'
+import Rect from '../../tools/Rect'
 
 const ToolBar = () => {
   return (
@@ -17,26 +18,26 @@ const ToolBar = () => {
       <button className='toolbar__btn brush' onClick={() => toolbarState.setTool(new Brush(canvasState.canvas))}>
         <img src={brush} alt="brush" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn rect' onClick={() => toolbarState.setTool(new Rect(canvasState.canvas))}>
         <img src={rect} alt="rect" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn circle'>
         <img src={circle} alt="circle" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn eraser'>
         <img src={eraser} alt="eraser" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn line'>
         <img src={line} alt="line" />
       </button>
       <input type="color" style={{marginLeft: 15}}/>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn undo'>
         <img src={undo} alt="undo" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn redo'>
         <img src={redo} alt="redo" />
       </button>
-      <button className='toolbar__btn brush'>
+      <button className='toolbar__btn save'>
         <img src={save} alt="save" />
       </button>
     </div>
